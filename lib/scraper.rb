bcbb849720e11d602693ed9279adc08eef4671b2
github username <github email address>
2019-01-19 12:42:56 -0800

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,48 +7,47 @@ class Scraper
   def self.scrape_index_page(index_url)
     url = File.read(index_url)
     doc = Nokogiri::HTML(url)
-    
+
     students = []
     doc.css("div.student-card").each do |student|
-     
+
       students += [
         :name => student.css("h4.student-name").text,
-        :location => student.css("p.student-location").text, 
+        :location => student.css("p.student-location").text,
         :profile_url => student.css("a").attribute("href").value
         ]
-    end 
+    end
     students
   end
 
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(File.read(profile_url))
-    
+
     student = {}
-    
+
     doc.css(".main-wrapper").each do |profile|
-      #binding.pry
-      social = profile.css(".social-icon-container a").map {|link| link["href"]} # or link("href").value
-      
-      if twitter_idx = social.index {|s|s.include?("twitter")}
-      elsif linkedin_idx = social.index {|s|s.include?("linkedin")}
-      elsif github_idx = social.index {|s|s.include?("github")}
-      elsif blog_index = social.index {|s|s.include?(".com")}
-      else
-        nil
+      links = profile.css(".social-icon-container a").map {|link| link["href"]}
+      # or link("href").value
+
+      links.select do |link|
+        if link.include?("twitter")
+          student[:twitter] = link
+        elsif link.include?("linkedin")
+          student[:linkedin] = link
+        elsif link.include?("github")
+          student[:github] = link
+        elsif link.include?(".com")
+          student[:blog] = link
+        end
       end
 
-      student = {
-        :twitter => social[twitter_idx],
-        :linkedin => social[linkedin_idx],
-        :github => social[github_idx],
-        :blog => social[blog_index],
-        :profile_quote => profile.css(".vitals-text-container .profile-quote").text,
-        :bio => profile.css(".details-container p").text
-      }
-       #profile.css(".social-icon-container //a[href*='twitter']"), -finding name of href but returns array
+      student[:profile_quote] = profile.css(".vitals-text-container .profile-quote").text
+      student[:bio] = profile.css(".details-container p").text
+
+      #finding name of href but returns array
+       #profile.css(".social-icon-container //a[href*='twitter']"),
     end
-    student.delete_if {|k,v|v.nil?}
+    student
   end
 
 end
-
